ebca031a2fe0e64f75cf4b318c1c5348b7671882
gary rong <garyrong0905@gmail.com>
2019-04-25 22:59:48 +0800

    all: integrate the freezer with fast sync
    * all: freezer style syncing
    
    core, eth, les, light: clean up freezer relative APIs
    
    core, eth, les, trie, ethdb, light: clean a bit
    
    core, eth, les, light: add unit tests
    
    core, light: rewrite setHead function
    
    core, eth: fix downloader unit tests
    
    core: add receipt chain insertion test
    
    core: use constant instead of hardcoding table name
    
    core: fix rollback
    
    core: fix setHead
    
    core/rawdb: remove canonical block first and then iterate side chain
    
    core/rawdb, ethdb: add hasAncient interface
    
    eth/downloader: calculate ancient limit via cht first
    
    core, eth, ethdb: lots of fixes
    
    * eth/downloader: print ancient disable log only for fast sync

--- core/state/sync.go
+++ core/state/sync.go
@@ -26,7 +26,7 @@ import (
 )
 
 // NewStateSync create a new state trie download scheduler.
-func NewStateSync(root common.Hash, database ethdb.Reader) *trie.Sync {
+func NewStateSync(root common.Hash, database ethdb.KeyValueReader) *trie.Sync {
 	var syncer *trie.Sync
 	callback := func(leaf []byte, parent common.Hash) error {
 		var obj Account
